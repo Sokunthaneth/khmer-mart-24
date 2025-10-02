13d7aa879322fd59cbbbdbce6995a8b4b627d59d
Sokunthaneth Chhoy <sokunthaneth.chhoy@nbc.gov.kh>
2025-10-02 15:58:15 +0700

    update RegisteredUserController

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,9 +36,11 @@ class RegisteredUserController extends Controller
         ]);
 
         $user = User::create([
+            'username' => $request->name,
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => 'user',
         ]);
 
         event(new Registered($user));
